227f076db2eee7d5f76107099471485f74e831c1
jili1998 <46920064+jili1998@users.noreply.github.com>
2019-10-22 21:21:36 +0800

    Add files via upload

---
+++ 语音部分/shibie.py
@@ -0,0 +1,47 @@
+import wav2pcm
+import requests
+import json
+import base64
+import os
+
+
+baidu_server = "https://openapi.baidu.com/oauth/2.0/token?"
+grant_type = "client_credentials"
+client_id = "Od5uRBIN3K26UG4pWuVY55hu"
+client_secret = "fYsv5tWpaoQqxihukTBOUanqB5tSGRLt" 
+
+url ="%sgrant_type=%s&client_id=%s&client_secret=%s"%(baidu_server,grant_type,client_id,client_secret)
+#print(url)
+
+res = requests.post(url)
+#print(res.text)
+token = json.loads(res.text)["access_token"]
+#print(token)
+
+RATE = "16000"
+FORMAT = "pcm"
+CUID="hsj_yuyin"
+DEV_PID="1536"
+pcm_file = wav2pcm.wav_to_pcm("/home/hsj/Documents/7.wav")
+with open(pcm_file,'rb') as f:
+	speech_data = f.read()
+
+size = len(speech_data)
+speech = base64.b64encode(speech_data).decode('utf8')
+headers = { 'Content-Type' : 'application/json'} 
+url = "https://vop.baidu.com/server_api"
+data={
+
+        "format":FORMAT,
+        "rate":RATE,
+        "dev_pid":DEV_PID,
+        "speech":speech,
+        "cuid":CUID,
+        "len":size,
+        "channel":1,
+        "token":token,
+    }
+
+req = requests.post(url,json.dumps(data),headers)
+result = json.loads(req.text)
+print(result["result"][0])
