cce31f572dd9cb8237767f15c87b25bedfae1963
jili1998 <46920064+jili1998@users.noreply.github.com>
2019-10-28 10:41:10 +0800

    Delete wav2pcm.py

--- 语音部分/wav2pcm.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def wav_to_pcm(wav_file):
-    pcm_file = "%s.pcm" %(wav_file.split(".")[0])
-
-    os.system("ffmpeg -y  -i %s  -acodec pcm_s16le -f s16le -ac 1 -ar 16000 %s"%(wav_file,pcm_file))
-
-    return pcm_file
-#将WAV格式文件转换为更适合百度Api所使用的pcm格式
